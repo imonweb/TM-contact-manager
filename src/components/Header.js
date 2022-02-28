7028d0293442082436ebb3e0d05bd86b3ac3b9c3
imonweb <contact@imonweb.co.uk>
2022-02-28 22:40:24 +0000

    css

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,8 @@ const Header = (props) => {
   const {branding} = props;
   return(
     <div>
-      <h1>{branding}</h1>
+      {/* <h1 style={{color: 'red', fontSize: '50px'}}>{branding}</h1> */}
+      <h1 style={headingStyle}>{branding}</h1>
     </div>
   )
 };
@@ -17,5 +18,10 @@ Header.defaultProps = {
 Header.propTypes = {
   branding: PropTypes.string.isRequired
 };
+
+const headingStyle = {
+  color: 'red', 
+  fontSize: '50px'
+}
 
 export default Header;
